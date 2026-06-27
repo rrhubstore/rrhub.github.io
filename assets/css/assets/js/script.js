5b0f2348d77523d0d4f5ee98b8b588490373a9c8
rrhub <rahulranarajput03@gmail.com>
2026-06-27 18:15:37 +0530

    Update script.js

--- assets/css/assets/js/script.js
+++ assets/css/assets/js/script.js
@@ -53,11 +53,12 @@ function searchProduct() {
         }
     }
 }
-let cart = [];
-
 function addToCart(name, price){
-cart.push({name, price});
-alert(name + " added to cart");
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-localStorage.setItem("cart", JSON.stringify(cart));
+    cart.push({name, price});
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(name + " added to cart");
 }
