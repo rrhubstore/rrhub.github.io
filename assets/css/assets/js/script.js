f12b4764c9df2c0b0f6cf492f8a2cc145fc4d0ee
rrhub <rahulranarajput03@gmail.com>
2026-06-27 21:11:14 +0530

    Update script.js

--- assets/css/assets/js/script.js
+++ assets/css/assets/js/script.js
@@ -181,3 +181,15 @@ function placeCODOrder(){
 
     alert("COD Order Placed ✅ Status: Pending");
 }
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
