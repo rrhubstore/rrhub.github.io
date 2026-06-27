b113c57624965c2f87c47e02ed05a8b1c2bb8328
rrhub <rahulranarajput03@gmail.com>
2026-06-27 18:12:43 +0530

    Add addToCart function to manage shopping cart

--- assets/css/assets/js/script.js
+++ assets/css/assets/js/script.js
@@ -53,3 +53,11 @@ function searchProduct() {
         }
     }
 }
+let cart = [];
+
+function addToCart(name, price){
+cart.push({name, price});
+alert(name + " added to cart");
+
+localStorage.setItem("cart", JSON.stringify(cart));
+}
